57afd87f99e38ebb7e3bbff938fb90fc6f073776
MIIMOmama <tkokmsk@gmail.com>
2022-09-13 13:31:58 +0900

    ボタンのmixinに幅指定をpaddingで調整ではなく、widthとheighで固定

--- _static/dist/js/index.js
+++ _static/dist/js/index.js
@@ -3,7 +3,7 @@
 window.onresize = function() {
     const windowSize = window.innerWidth;
 
-    if(window.matchMedia("max-width: 400px").matches) {
+    if(window.matchMedia("max-width: 480px").matches) {
     document.querySelector('.global-nav').style.transition = '.5s';
     document.querySelector('.global-header__button').style.transition = '.5s';
     } else {
